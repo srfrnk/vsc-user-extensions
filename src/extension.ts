42f7fd9577193953172411814e011c074ced3803
srfrnk <srfrnk@gmail.com>
2017-06-25 09:08:04 +0300

    install+ uninstall

--- src/extension.ts
+++ src/extension.ts
@@ -4,56 +4,69 @@
 import * as vscode from 'vscode';
 // var spawn = require('cross-spawn');
 const { spawn } = require('child_process');
+const output: vscode.OutputChannel = vscode.window.createOutputChannel('User-Extensions');
 
 // this method is called when your extension is activated   
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
     let recommendations = vscode.workspace.getConfiguration('extensions').recommendations;
-    let installed: Array<string> = vscode.extensions.all.map(ext => ext.id);
-
-    let install = recommendations.filter(ext => !installed.includes(ext));
-    Promise.all(install.map(ext => {
-        return new Promise((res, rej) => {
-            const cmd = spawn('code', ['--install-extension', ext]);
-            cmd.stdout.on('data', (data) => console.log(data));
-            cmd.stderr.on('data', (data) => console.log(data));
-            cmd.on('close', (code) => {
-                res(code);
-            });
-        });
-    })).then(() => {
-        console.log("Installed new extensions");
-    })
-
-    // const cmd = spawn('code', ['--install-extension', '']);
-
-    // cmd.stdout.on('data', (data) => {
-    //     console.log(`stdout: ${data}`);
-    // });
-
-    // cmd.stderr.on('data', (data) => {
-    //     console.log(`stderr: ${data}`);
-    // });
-
-    // cmd.on('close', (code) => {
-    //     console.log(`child process exited with code ${code}`);
-    // });
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        console.log('ddddd');
-        vscode.window.showInformationMessage('Hello World!');
+    getExtensions().then(installed => {
+        installAll(recommendations, installed);
+        uninstallAll(recommendations, installed);
+    }).then(() => {
+        output.append('Done');
     });
-
-    context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+}
+
+function getExtensions(): Promise<Array<string>> {
+    return new Promise((res, rej) => {
+        let extensions = "";
+        const cmd = spawn('code', ['--list-extensions']);
+        cmd.stdout.on('data', (data) => extensions += data.toString());
+        cmd.stderr.on('data', (data) => console.log(data.toString()));
+        cmd.on('close', (code) => {
+            res(extensions.split('\n').filter(e => e.length > 0));
+        });
+    });
+}
+
+function installAll(recommendations: Array<string>, installed: Array<string>) {
+    let install = recommendations.filter(ext => !installed.includes(ext));
+    if (install.length > 0) {
+        Promise.all(install.map(ext => {
+            return new Promise((res, rej) => {
+                const cmd = spawn('code', ['--install-extension', ext]);
+                cmd.stdout.on('data', (data) => console.log(data.toString()));
+                cmd.stderr.on('data', (data) => console.log(data.toString()));
+                cmd.on('close', (code) => {
+                    res(code);
+                });
+            });
+        })).then(() => {
+            output.append('Installed new extensions - Please reload window');
+        });
+    }
+}
+
+function uninstallAll(recommendations: Array<string>, installed: Array<string>) {
+    let uninstall = installed.filter(ext => !recommendations.includes(ext));
+    if (uninstall.length > 0) {
+        Promise.all(uninstall.map(ext => {
+            return new Promise((res, rej) => {
+                output.append(`Uninstalling ${ext}`);
+                const cmd = spawn('code', ['--uninstall-extension', ext]);
+                cmd.stdout.on('data', (data) => console.log(data.toString()));
+                cmd.stderr.on('data', (data) => console.log(data.toString()));
+                cmd.on('close', (code) => {
+                    res(code);
+                });
+            });
+        })).then(() => {
+            output.append('Uninstalled old extensions - Please reload window');
+        });
+    }
 }
